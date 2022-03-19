bc930f0bb80774d016ea1090e347993ee6cab64f
Azamat <az1k3@outlook.com>
2022-03-19 13:29:41 +0500

    Added Controllers

---
+++ MovieTickets/Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace MovieTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
---
+++ MovieTickets/Controllers/CinemasController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace MovieTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
