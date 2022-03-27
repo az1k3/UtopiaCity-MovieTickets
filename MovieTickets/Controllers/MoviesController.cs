a590b0e7fa020a2753a6e9dc2a1e2d80a6cfe619
Azamat <az1k3@outlook.com>
2022-03-27 15:07:01 +0500

    Added search filter

--- MovieTickets/Controllers/MoviesController.cs
+++ MovieTickets/Controllers/MoviesController.cs
@@ -24,6 +24,19 @@ namespace MovieTickets.Controllers
             return View(allMovies);
         }
 
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allMovies = await _service.GetAllAsync();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
+                return View("Index",filteredResult);
+            }
+
+            return View("Index", allMovies);
+        }
+
         //GET: Movies/Details/Id
         public async Task<IActionResult> Details (int id)
         {
