9a0d87fca43927030a2b849cae0cffccc2554290
Azamat <az1k3@outlook.com>
2022-03-30 19:47:20 +0500

    Added list of users, orders list

--- MovieTickets/Controllers/AccountController.cs
+++ MovieTickets/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Data.Static;
 using DataAccessLayer.Contexts;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace MovieTickets.Controllers
@@ -19,6 +20,11 @@ namespace MovieTickets.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
             _context = context;
+        }
+        public async Task<IActionResult> Users()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
         }
 
         public IActionResult Login() => View(new LoginVM());
